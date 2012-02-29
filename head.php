8994055fddf5311f0a4b66fbc83c221ce6e89a90
rossannamarie <rcatahan@mail.umw.edu>
2012-02-29 15:19:13 -0500

    modified head file

--- head.php
+++ head.php
@@ -12,21 +12,17 @@
 
 <body>
 	<div id="header">
-		<a href="index.html" id="logo"><img src="images/logo.jpg" alt="LOGO" /></a>
+		<a href="index.php" id="logo"><img src="images/logo.jpg" alt="LOGO" /></a>
 		<div id="navigation">
 			<ul>
 				<li class="first selected"><a href="index.php">Home</a></li>
 				<li><a href="leaveform.php">Leave Form</a></li>
-				<li><a href="services.html">Services</a></li>
-				<li><a href="solutions.html">Solutions</a></li>
-				<li><a href="support.html">Support</a></li>
-				<li><a href="blog.html">Blog</a></li>
-				<li><a href="contact.html">Contact</a></li>
+				<li><a href="login.php">Supervisor Login</a></li>
 			</ul>
 		</div>
 		<div id="search">
 			<form action="" method="">
-				<input type="text" value="Search" class="txtfield" onblur="javascript:if(this.value==''){this.value=this.defaultValue;}" onfocus="javascript:if(this.value==this.defaultValue){this.value='';}" />
+				<input type="text" value="Search avatar name" class="txtfield" onblur="javascript:if(this.value==''){this.value=this.defaultValue;}" onfocus="javascript:if(this.value==this.defaultValue){this.value='';}" />
 				<input type="submit" value="" class="button" />
 			</form>
 		</div>
