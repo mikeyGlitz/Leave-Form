52d4495a758972bfc00a63688268e2c333a4eb00
Michael Glitzos <mglitzos@rosemary.umw.edu>
2012-02-29 15:15:03 -0500

    Organized the Footer text

--- footer.php
+++ footer.php
@@ -1,26 +1,14 @@
 <div id="footer">
-                <ul class="contacts">
-                        <h3>Contact Us</h3>
-                        <li><span>Email</span><p>: company@email.com</p></li>
-                        <li><span>Address</span><p>: 189 Lorem Ipsum Pellentesque, Mauris Etiam ut velit odio Proin id nisi enim 0000</p></li>
-                        <li><span>Phone</span><p>: 117-683-9187-000</p></li>
-                </ul>
-                <ul id="connect">
-                        <h3>Get Updated</h3>
-                        <li><a href="blog.html">Blog</a></li>
-                        <li><a href="http://facebook.com/freewebsitetemplates" target="_blank">Facebook</a></li>
-                        <li><a href="http://twitter.com/fwtemplates" target="_blank">Twitter</a></li>
-                </ul>
-                <div id="newsletter">
-                        <p><b>Sign-up for Newsletter</b>
-                                In sollicitudin vulputate metus, sed commodo diam elementum nec. Sed et risus sed magna convallis adipiscing.
-                        </p>
-                        <form action="" method="">
-                                <input type="text" value="Name" class="txtfield" onblur="javascript:if(this.value==''){this.value=this.defaultValue;}" onfocus="javascript:if(this.value==this.defaultValue){this.value='';}" />
-                                <input type="text" value="Enter Email Address" class="txtfield" onblur="javascript:if(this.value==''){this.value=this.defaultValue;}" onfocus="javascript:if(this.value==this.defaultValue){this.value='';}" />
-                                <input type="submit" value="" class="button" />
-                        </form>
-                </div>
+                <table class="contacts" style="float: left">
+                        <tr><td><h3>Contact Us</h3></td></tr>
+                        <tr><td><b>Address:</b></td><td> <b>University of Mary Washington Payroll Services</b><br /> 1301 College Ave, Fredericksburg, VA 22401</td></tr>
+                        <tr><td>Phone:</td><td> 117-683-9187-000</td></tr>
+                </table>
+		<table class="contacts" style="float: left">
+                        <tr><td><h3>Contact Development Team</h3></td></tr>
+                        <tr><td><b>Email:</b></td><td>UMWLeave@gmail.com</td></tr>
+                </table>
+
                 <span class="footnote">&copy; Copyright &copy; 2011. All rights reserved</span>
         </div> <!-- /#footer -->
 
