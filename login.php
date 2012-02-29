d0b8b87efce1f81c69361629e9b6708ed60c5ad9
Michael Glitzos <mglitzos@rosemary.umw.edu>
2012-02-29 15:21:17 -0500

    Made login.php compliant to template

--- login.php
+++ login.php
@@ -1,1 +1,26 @@
-//Add login form for supervisors here
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Business Solutions</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />	
+	<!--[if lte IE 7]>
+		<link rel="stylesheet" href="css/ie.css" type="text/css" charset="utf-8" />	
+	<![endif]-->
+</head>
+
+<body>
+	<?php
+		include "head.php";
+	?>
+	<div id="contents">
+		<div class="background">
+			<h1>More to Come Soon</h1>
+		</div>
+	</div> <!-- /#contents -->
+	<?php
+		include "footer.php";
+	?>
+</body>
+</html>
