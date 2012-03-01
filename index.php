c346f8825ba7eef5eda5ecdd974dd57f7b2bd7c7
Michael Glitzos <mglitzos@rosemary.umw.edu>
2012-03-01 15:04:05 -0500

    Added email capabilities to the main page

--- index.php
+++ index.php
@@ -3,19 +3,22 @@
 ?>
 	<div id="contents">
 		<div class="background" style="min-height: 150px; text-align: center">
-			<h2>Select Your Employment Status</h2><br />
+			<h2>Verify Employment Information</h2><br />
 			<hr /><br />
 			<?php 
 			if($_GET['msg'] != ''){ ?>
 			<font style="color: red; font-weight: bold; font-size: 16pt;">***<?php echo $_GET['msg']; ?>***</font>
 			<?php } ?>
-			<form name=employment id=employee action="leaveform.php" method="get" onsubmit="return validateForm('empl')">
+			<form name=employment id=employee action="leaveform.php" method="post" onsubmit="return validateForm('empl')">
+				<label for=email>Email Address:</label>
+				<input type=text name=email /><br />
+				<label for=empl>Employment Type:</label>
 				<select name=empl id=empl>
 					<option selected>---Select an option---</option>
 					<option name=adminfac id=adminfac>Administrative Faculty</option>
 					<option name=clasempl id=clasempl>Classified Employee</option>
 					<option name=partempl id=partempl>Part-Time Employee</option>
-				</select>
+				</select><br />
 				<input type=submit value="proceed" />
 			</form>
 		</div>
