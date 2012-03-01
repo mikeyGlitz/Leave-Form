8cc68cae719d97b03369587029e13758e8022afb
Michael Glitzos <mglitzos@rosemary.umw.edu>
2012-03-01 02:17:27 -0500

    more changes

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-<div id="footer" style="clear:both">
+<div id="footer">
                 <table class="contacts" style="float: left; margin-right: 2em">
-                        <tr><td><h3>Contact Us</h3></td></tr>
+                        <tr><td><h3><a href="adminfinance.umw.edu/payroll/contact" style="text-decoration: none; color: lightblue">Contact Us</a></h3></td></tr>
                         <tr><td><b>Address:</b></td><td> <b>University of Mary Washington Payroll Services</b><br /> 1301 College Ave, Fredericksburg, VA 22401</td></tr>
                         <tr><td>Phone:</td><td> 117-683-9187-000</td></tr>
                 </table>
 		<table class="contacts" style="float: left">
-                        <tr><td><h3>Contact Development Team</h3></td></tr>
+                        <tr><td><h3 color=lightblue>Contact Development Team</h3></td></tr>
                         <tr><td><b>Email:</b></td><td>UMWLeave@gmail.com</td></tr>
                 </table>
 
