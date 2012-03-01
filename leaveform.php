f55721fc950098b9eb9b1966a091c1591b81b9d0
Michael Glitzos <mglitzos@rosemary.umw.edu>
2012-02-29 21:49:09 -0500

    Made the code more readable

--- leaveform.php
+++ leaveform.php
@@ -17,17 +17,17 @@
 	<div id="contents">
 		<div class="background">
 			<h2>Select Your Employment Status</h2><br />
-<hr /><br />
-<form name=employment id=employee>
-<select name=empl id=empl>
-<option selected>---Select an option---</option>
-<option name=adminfac id=adminfac>Administrative Faculty</option>
-<option name=clasempl id=clasempl>Classified Employee</option>
-<option name=partempl id=partempl>Part-Time Employee</option>
-<option name=altpay id=altpay>Alternate Pay</option>
-</select>
-<input type=submit value="proceed" />
-</form>
+			<hr /><br />
+			<form name=employment id=employee>
+				<select name=empl id=empl>
+					<option selected>---Select an option---</option>
+					<option name=adminfac id=adminfac>Administrative Faculty</option>
+					<option name=clasempl id=clasempl>Classified Employee</option>
+					<option name=partempl id=partempl>Part-Time Employee</option>
+					<option name=altpay id=altpay>Alternate Pay</option>
+				</select>
+				<input type=submit value="proceed" />
+			</form>
 		</div>
 	</div> <!-- /#contents -->
 	<?php
