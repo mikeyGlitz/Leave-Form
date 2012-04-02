a4f742ade91f1a8fd63f854620b0d996d1dac0ce
Michael Glitzos <mglitzos@mail.umw.edu>
2012-04-02 15:32:59 -0400

    Added a javascript to check the form

--- index.php
+++ index.php
@@ -4,15 +4,26 @@
 
 	include "head.php";
 ?>
+	<script type=text/javascript language=javascript>
+		function formValidate(field1, field2){
+			if(document.getElementsByName(field1)[0].value.length < 4){
+				alert('Please enter a valid email address');
+				return false;
+			}else
+			if(document.getElementsByName(field2)[0].value == '---Select an option---'){
+				alert('Please select a valid employee type');
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
+	</script>
 	<div id="contents">
 		<div class="background" style="min-height: 150px; text-align: center">
 			<h2>Verify Employment Information</h2><br />
 			<hr /><br />
-			<?php 
-			if($_GET['msg'] != ''){ ?>
-			<font style="color: red; font-weight: bold; font-size: 16pt;">***<?php echo $_GET['msg']; ?>***</font>
-			<?php } ?>
-			<form name=employment id=employee action="leaveform.php" method="post" onsubmit="return validateForm('empl')">
+			<form name=employment id=employee action="leaveform.php" method="post" onsubmit="return formValidate('email', 'empl')">
 				<label for=email>Email Address:</label>
 				<input type=text name=email /><br />
 				<label for=empl>Employment Type:</label>
