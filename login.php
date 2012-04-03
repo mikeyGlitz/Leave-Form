fa0c3deb25f8338e2f32e9a94e669854412bbedd
Michael Glitzos <mglitzos@mail.umw.edu>
2012-04-03 14:58:27 -0400

    Fixed the login page so the code is no longer broken. Form Validaiton has also been added

--- login.php
+++ login.php
@@ -1,25 +1,30 @@
-<!DOCTYPE html>
-<!-- Website template by freewebsitetemplates.com -->
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Business Solutions</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />	
-	<!--[if lte IE 7]>
-		<link rel="stylesheet" href="css/ie.css" type="text/css" charset="utf-8" />	
-	<![endif]-->
-</head>
-
-<body>
-	<?php
-		include "head.php";
-	?>
+<?php
+	include "head.php";
+?>
+<script type=text/javascript language=javascript>
+	function formValidate(field1, field2){
+		var x = document.getElementsByName(field1)[0].value;
+		var atpos = x.indexOf('@');
+		var dotpos = x.indexOf('.');
+		if(atpos < 1 || dotpos < atpos + 2 || dotpos +2 >= x.length){
+			alert("Not a valid email address.");
+			return false;
+		}
+		else if(document.getElementsByName(field2)[0].value.length < 4){
+			alert("Not a valid password");
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+</script>
 	<div id="contents">
 		<div class="background" style="text-align: center; min-height: 150px;">
 			<h1>Login</h1>
 			<hr />
 			<h2><b><? echo $get['msg']; ?></b></h2>
-			<form action=loginController.php method=post>
+			<form action=loginController.php method=post onsubmit="return formValidate('email', 'password')">
 			<!--<form action=view.php method=post>-->
 				<label for=email>Email:</label><input type=text name=email /><br />
 				<label for=password>Password:</form><input type=password name=password /><br />
