35bb577f2face3614d83a8750667cc85403e914b
rossannamarie <rcatahan@mail.umw.edu>
2012-04-18 14:37:59 -0400

    changes password

--- recover.php
+++ recover.php
@@ -6,11 +6,13 @@
 	$password = $_POST['password'];
 
 	//Create a query string
-	$q1 = "SELECT key FROM super WHERE email = \'$user\';";
+	$q1 = "SELECT key FROM super WHERE email = '$user';";
 
-	$q2 = "UPDATE super SET password = \'$password\' WHERE email = \'$user\';";
+	$q2 = "UPDATE super SET password = '$password' WHERE email = '$user';";
 
-	if(mysqli_query($link, $q1)){
+	echo $q1.", ".$q2;
+
+	//if(mysqli_query($link, $q1)){
 		$result = mysqli_query($link, $q2) or die("Could not execute query");
-	}
+	//}
 ?>
